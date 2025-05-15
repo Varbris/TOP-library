5a1dd3707c1ce6a9510b507f97177a0265cc89ac
varbris <92345143+Varbris@users.noreply.github.com>
2025-05-15 17:28:59 +0700

    Add book function and constructor

--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+const myLibrary = [];
+
+function Book(title, author, pagesNumber) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pagesNumber = pagesNumber;
+  this.info = function () {
+    var bookInfo =
+      this.title +
+      " By " +
+      this.author +
+      ", " +
+      this.pagesNumber +
+      " pages, not read yet";
+    return bookInfo;
+  };
+}
+
+function addBookToLibrary(title, author, pagesNumber) {
+  //take params, create a book then store it in the array
+  const book = new Book(title, author, pagesNumber);
+  myLibrary.push(book);
+}
+
+addBookToLibrary("awikwok", "aku", 100);
